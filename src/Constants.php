cbe73ecdc7b5d9e4f38313eb1c0d6443d046474b
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2022-08-23 00:56:41 +0430

    New release

--- src/Constants.php
+++ src/Constants.php
@@ -5,6 +5,6 @@ namespace Sakoo\Framework\Core;
 class Constants
 {
 	public const FRAMEWORK_NAME = 'Sakoo Framework';
-	public const FRAMEWORK_VERSION = '0.2.0';
+	public const FRAMEWORK_VERSION = '0.2.1';
 	public const AUTHOR = 'Pouya Asgharnejad Tehran';
 }
