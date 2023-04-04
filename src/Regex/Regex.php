e1bc2268e7499329865d84ac239b171632acf74d
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2023-04-05 02:01:40 +0330

    Add regex class

--- src/Regex/Regex.php
+++ src/Regex/Regex.php
@@ -1,1 +1,228 @@
 <?php
+
+namespace Sakoo\Framework\Core\Regex;
+
+class Regex
+{
+	public const ALPHABET_UPPER = 'A-Z';
+	public const ALPHABET_LOWER = 'a-z';
+	public const DIGITS = '0-9';
+	public const UNDERLINE = '_';
+	public const DOT = '.';
+
+	private function __construct(private string $pattern = '')
+	{
+	}
+
+	public static function make(): static
+	{
+		return new static();
+	}
+
+	public function append(string $value): static
+	{
+		$this->add($this->escapeChars($value));
+		return $this;
+	}
+
+	public function add(string $value): static
+	{
+		$this->pattern .= $value;
+		return $this;
+	}
+
+	public function startOfLine(): static
+	{
+		$this->add('^');
+		return $this;
+	}
+
+	public function endOfLine(): static
+	{
+		$this->add('$');
+		return $this;
+	}
+
+	public function startsWith(string|callable $value): static
+	{
+		$this->startOfLine();
+		$this->callOrAppend($value);
+		return $this;
+	}
+
+	public function endsWith(string|callable $value): static
+	{
+		$this->callOrAppend($value);
+		$this->endOfLine();
+		return $this;
+	}
+
+	public function digit(int $length = 0): static
+	{
+		$this->add('\d' . ($length > 0 ? '{' . $length . '}' : ''));
+		return $this;
+	}
+
+	public function oneOf(array $value): static
+	{
+		$this->wrap(fn ($exp) => $this->add(implode('|', $value)), true);
+		return $this;
+	}
+
+	public function wrap(callable $fn, bool $nonCapturing = false): static
+	{
+		$this->add('(' . ($nonCapturing ? '?:' : ''));
+		$fn($this);
+		$this->add(')');
+		return $this;
+	}
+
+	public function bracket(callable $fn): static
+	{
+		$this->add('[');
+		$fn($this);
+		$this->add(']');
+		return $this;
+	}
+
+	public function maybe(string $value): static
+	{
+		$this->append($value);
+		$this->add('?');
+		return $this;
+	}
+
+	public function anything(): static
+	{
+		$this->add('.*');
+		return $this;
+	}
+
+	public function something(): static
+	{
+		$this->add('.+');
+		return $this;
+	}
+
+	public function unixLineBreak(): static
+	{
+		$this->append('\n');
+		return $this;
+	}
+
+	public function windowsLineBreak(): static
+	{
+		$this->append('\r\n');
+		return $this;
+	}
+
+	public function tab(): static
+	{
+		$this->append('\t');
+		return $this;
+	}
+
+	public function space(): static
+	{
+		$this->append('\s');
+		return $this;
+	}
+
+	public function word(): static
+	{
+		$this->append('\w');
+		return $this;
+	}
+
+	public function chars(...$values): static
+	{
+		$this->add(implode('', $values));
+		return $this;
+	}
+
+	public function anythingWithout(string|callable $value): static
+	{
+		$this->add('[^');
+		$this->callOrAppend($value);
+		$this->add(']*');
+		return $this;
+	}
+
+	public function somethingWithout(string|callable $value): static
+	{
+		$this->add('[^');
+		$this->callOrAppend($value);
+		$this->add(']+');
+		return $this;
+	}
+
+	public function anythingWith(string|callable $value): static
+	{
+		$this->add('[');
+		$this->callOrAppend($value);
+		$this->add(']*');
+		return $this;
+	}
+
+	public function somethingWith(string|callable $value): static
+	{
+		$this->add('[');
+		$this->callOrAppend($value);
+		$this->add(']+');
+		return $this;
+	}
+
+	private function callOrAppend(string|callable $value): void
+	{
+		if (is_callable($value)) {
+			$value($this);
+		}
+
+		if (is_string($value)) {
+			$this->append($value);
+		}
+	}
+
+	public function escapeChars(string $value): string
+	{
+		if (empty($value)) {
+			return '';
+		}
+
+		return preg_quote($value, '/');
+	}
+
+	public function match(string $value): array
+	{
+		$matches = null;
+		preg_match("/$this->pattern/", $value, $matches);
+		return $matches;
+	}
+
+	public function matchAll(string $value): array
+	{
+		$matches = null;
+		preg_match_all("/$this->pattern/", $value, $matches);
+		return $matches;
+	}
+
+	public function test(string $value): bool
+	{
+		return !empty($this->match($value));
+	}
+
+	public function replace(string $string, string $replace): mixed
+	{
+		return preg_replace("/$this->pattern/", $replace, $string);
+	}
+
+	public function get(): string
+	{
+		return $this->pattern;
+	}
+
+	public function __toString(): string
+	{
+		return $this->get();
+	}
+}
--- tests/Regex/RegexTest.php
+++ tests/Regex/RegexTest.php
@@ -1,1 +1,101 @@
 <?php
+
+namespace Sakoo\Framework\Core\Tests\Regex;
+
+use Sakoo\Framework\Core\Regex\Regex;
+use Sakoo\Framework\Core\Tests\TestCase;
+
+class RegexTest extends TestCase
+{
+	public function test_it_returns_full_regex_by_get_function()
+	{
+		$this->assertEquals('^', Regex::make()->startOfLine()->get());
+		$this->assertEquals('^A', Regex::make()->startsWith('A')->get());
+
+		$this->assertEquals('$', Regex::make()->endOfLine()->get());
+		$this->assertEquals('Z$', Regex::make()->endsWith('Z')->get());
+
+		$this->assertEquals('.*', Regex::make()->anything()->get());
+		$this->assertEquals('.+', Regex::make()->something()->get());
+
+		$this->assertEquals('[ABC]+', Regex::make()->somethingWith('ABC')->get());
+		$this->assertEquals('[ABC]*', Regex::make()->anythingWith('ABC')->get());
+		$this->assertEquals('[^ABC]+', Regex::make()->somethingWithout('ABC')->get());
+		$this->assertEquals('[^ABC]*', Regex::make()->anythingWithout('ABC')->get());
+
+		$this->assertEquals('(ABC)', Regex::make()->wrap(fn (Regex $exp) => $exp->append('ABC'))->get());
+		$this->assertEquals('(?:ABC)', Regex::make()->wrap(fn (Regex $exp) => $exp->append('ABC'), true)->get());
+
+		$this->assertEquals('[ABC]', Regex::make()->bracket(fn (Regex $exp) => $exp->append('ABC'))->get());
+
+		$this->assertEquals('\/', Regex::make()->append('/')->get());
+		$this->assertEquals('/', Regex::make()->add('/')->get());
+
+		$this->assertEquals('\\\\r\\\\n', Regex::make()->windowsLineBreak()->get());
+		$this->assertEquals('\\\\n', Regex::make()->unixLineBreak()->get());
+		$this->assertEquals('\\\\t', Regex::make()->tab());
+		$this->assertEquals('\\\\s', Regex::make()->space());
+		$this->assertEquals('\\\\w', Regex::make()->word());
+
+		$this->assertEquals('A?', Regex::make()->maybe('A'));
+
+		$this->assertEquals('(?:A|B|C)', Regex::make()->oneOf(['A', 'B', 'C']));
+
+		$this->assertEquals('\d{3}', Regex::make()->digit(3));
+
+		$this->assertEquals('A-Z', Regex::make()->chars(Regex::ALPHABET_UPPER)->get());
+		$this->assertEquals('A-Za-z', Regex::make()->chars(Regex::ALPHABET_UPPER, Regex::ALPHABET_LOWER)->get());
+		$this->assertEquals('A-Za-z0-9', Regex::make()->chars(Regex::ALPHABET_UPPER, Regex::ALPHABET_LOWER, Regex::DIGITS)->get());
+
+		$this->assertEquals('', Regex::make()->escapeChars(''));
+		$this->assertEquals('\<', Regex::make()->escapeChars('<'));
+	}
+
+	public function test_it_can_test_matching_expression()
+	{
+		$regex = Regex::make()
+			->startsWith('09')
+			->digit(9)
+			->endOfLine();
+
+		$this->assertTrue($regex->test('09123456789'));
+		$this->assertFalse($regex->test('01122334455'));
+	}
+
+	public function test_it_can_replace_matching_expression()
+	{
+		$regex = Regex::make()
+			->append('<')
+			->wrap(fn ($exp) => $exp->maybe('/'))
+			->somethingWithout('>')
+			->append('>');
+
+		$this->assertEquals('<h2>Hello World</h2>', $regex->replace('<h1>Hello World</h1>', '<$1h2>'));
+	}
+
+	public function test_it_can_find_matching_expression()
+	{
+		$str = <<<'REGEXP'
+            test@gmail.com
+            foo@dev.net
+            icloud.com
+            user@yahoo.org
+        REGEXP;
+
+		$regex = Regex::make()
+			->somethingWith(fn ($exp) => $exp->chars(
+				Regex::ALPHABET_UPPER,
+				Regex::ALPHABET_LOWER,
+				Regex::DIGITS,
+				Regex::UNDERLINE,
+				Regex::DOT,
+			))
+			->append('@')
+			->oneOf(['gmail', 'yahoo', 'icloud'])
+			->append('.')
+			->oneOf(['com', 'org', 'net']);
+
+		$this->assertEquals('test@gmail.com', $regex->match($str)[0]);
+		$this->assertEqualsCanonicalizing(['test@gmail.com', 'user@yahoo.org'], $regex->matchAll($str)[0]);
+	}
+}
