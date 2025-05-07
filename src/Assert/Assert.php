b37f2a9203f06c749fe1b29639f5d4a083eba862
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2025-05-07 22:50:29 +0330

    feat: add assert module

---
+++ src/Assert/Assert.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sakoo\Framework\Core\Assert;
+
+use Sakoo\Framework\Core\Assert\Exception\InvalidArgumentException;
+use Sakoo\Framework\Core\Assert\Traits\BooleanType;
+use Sakoo\Framework\Core\Assert\Traits\CallableType;
+use Sakoo\Framework\Core\Assert\Traits\FileType;
+use Sakoo\Framework\Core\Assert\Traits\GeneralType;
+use Sakoo\Framework\Core\Assert\Traits\NullType;
+use Sakoo\Framework\Core\Assert\Traits\NumberType;
+use Sakoo\Framework\Core\Assert\Traits\ObjectType;
+use Sakoo\Framework\Core\Assert\Traits\ResourceType;
+use Sakoo\Framework\Core\Assert\Traits\ScalarType;
+use Sakoo\Framework\Core\Assert\Traits\StringType;
+use Sakoo\Framework\Core\Assert\Traits\TraversableType;
+
+class Assert
+{
+	use BooleanType;
+	use CallableType;
+	use FileType;
+	use GeneralType;
+	use NullType;
+	use NumberType;
+	use ObjectType;
+	use ResourceType;
+	use ScalarType;
+	use StringType;
+	use TraversableType;
+
+	public static function that(mixed $value): AssertionChain
+	{
+		return new AssertionChain($value);
+	}
+
+	public static function lazy(): LazyAssertion
+	{
+		return new LazyAssertion();
+	}
+
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	protected static function throwIf(bool $condition, string $message = ''): void
+	{
+		static::throwUnless(!$condition, $message);
+	}
+
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	protected static function throwUnless(bool $condition, string $message = ''): void
+	{
+		$condition ?: throw new InvalidArgumentException($message);
+	}
+}
---
+++ src/Assert/Traits/ObjectType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sakoo\Framework\Core\Assert\Traits;
+
+use Sakoo\Framework\Core\VarDump\VarDump;
+
+trait ObjectType
+{
+	public static function object(mixed $value, string $message = ''): void
+	{
+		$message = $message ?: sprintf('Given value %s is not an object', new VarDump($value));
+		static::throwUnless(is_object($value), $message);
+	}
+
+	public static function notObject(mixed $value, string $message = ''): void
+	{
+		$message = $message ?: sprintf('Given value %s is an object', new VarDump($value));
+		static::throwIf(is_object($value), $message);
+	}
+
+	public static function instanceOf(mixed $value, string $class, string $message = ''): void
+	{
+		$message = $message ?: sprintf('Given value %s is not instance of %s', new VarDump($value), $class);
+		static::throwUnless((is_string($value) || is_object($value)) && is_subclass_of($value, $class), $message);
+	}
+
+	public static function notInstanceOf(mixed $value, string $class, string $message = ''): void
+	{
+		$message = $message ?: sprintf('Given value %s is instance of %s', new VarDump($value), $class);
+		static::throwIf((is_string($value) || is_object($value)) && is_subclass_of($value, $class), $message);
+	}
+}
