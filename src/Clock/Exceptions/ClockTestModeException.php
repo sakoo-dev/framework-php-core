0637a2303002740b7e56df5500cafd8b741c1880
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2025-05-09 01:54:25 +0330

    feat: add clock module
    Signed-off-by: Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>

--- src/Clock/Exceptions/ClockTestModeException.php
+++ src/Clock/Exceptions/ClockTestModeException.php
@@ -1,9 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sakoo\Framework\Core\Clock\Exceptions;
 
 use Sakoo\Framework\Core\Exception\Exception;
 
-class ClockTestModeException extends Exception
-{
-}
+class ClockTestModeException extends Exception {}
