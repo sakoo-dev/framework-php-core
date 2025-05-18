96f2ba900216d0bc756d375ef01a7c78fbb05d38
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2025-05-18 23:05:30 +0330

    feat: add env module
    Signed-off-by: Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>

--- tests/Env/EnvTest.php
+++ tests/Env/EnvTest.php
@@ -1,21 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sakoo\Framework\Core\Tests\Env;
 
+use PHPUnit\Framework\Attributes\Test;
 use Sakoo\Framework\Core\Env\Env;
 use Sakoo\Framework\Core\FileSystem\Disk;
 use Sakoo\Framework\Core\FileSystem\File;
+use Sakoo\Framework\Core\FileSystem\Storage;
 use Sakoo\Framework\Core\Path\Path;
 use Sakoo\Framework\Core\Tests\TestCase;
 
-class EnvTest extends TestCase
+final class EnvTest extends TestCase
 {
-	private string $dotEnvPath;
+	private Storage $storage;
 
 	protected function setUp(): void
 	{
 		parent::setUp();
-		$this->dotEnvPath = Path::getStorageDir() . '/tests/env/.env';
+		$path = Path::getTempTestDir() . '/env/.env';
+		$this->storage = File::open(Disk::Local, $path);
 	}
 
 	protected function tearDown(): void
@@ -24,7 +29,8 @@ class EnvTest extends TestCase
 		$this->removeEnvVariables('FOO', 'BAR', 'BAZ');
 	}
 
-	public function test_it_can_get_from_env()
+	#[Test]
+	public function it_can_get_from_env(): void
 	{
 		$this->assertNull(Env::get('FOO'));
 
@@ -33,7 +39,8 @@ class EnvTest extends TestCase
 		$this->assertEquals('BAR', Env::get('FOO'));
 	}
 
-	public function test_it_can_return_default_when_key_not_exists()
+	#[Test]
+	public function it_can_return_default_when_key_not_exists(): void
 	{
 		$this->assertEquals('BAZ', Env::get('FOO', 'BAZ'));
 
@@ -42,58 +49,52 @@ class EnvTest extends TestCase
 		$this->assertEquals('BAR', Env::get('FOO', 'BAZ'));
 	}
 
-	public function test_it_can_load_configs_from_file()
+	#[Test]
+	public function it_can_load_configs_from_file(): void
 	{
-		$content = "\n\n FOO=VALUE1 \n BAR=VALUE2 \n BAZ=VALUE3 \n\n";
-		$this->makeDotEnvFile($content);
+		$this->storage->write("\n\n FOO=VALUE1 \n BAR=VALUE2 \n BAZ=VALUE3 \n\n");
 
 		$this->assertNull(Env::get('FOO'));
 		$this->assertNull(Env::get('BAR'));
 		$this->assertNull(Env::get('BAZ'));
 
-		Env::load($this->dotEnvPath);
+		Env::load($this->storage);
 
 		$this->assertEquals('VALUE1', Env::get('FOO'));
 		$this->assertEquals('VALUE2', Env::get('BAR'));
 		$this->assertEquals('VALUE3', Env::get('BAZ'));
 	}
 
-	public function test_it_ignore_commented_configs()
+	#[Test]
+	public function it_ignore_commented_configs(): void
 	{
-		$content = "#FOO=VALUE1 \n # BAR=VALUE2 \n BAZ=VALUE3";
-		$this->makeDotEnvFile($content);
+		$this->storage->write("#FOO=VALUE1 \n # BAR=VALUE2 \n BAZ=VALUE3");
 
 		$this->assertNull(Env::get('FOO'));
 		$this->assertNull(Env::get('BAR'));
 		$this->assertNull(Env::get('BAZ'));
 
-		Env::load($this->dotEnvPath);
+		Env::load($this->storage);
 
 		$this->assertNull(Env::get('FOO'));
 		$this->assertNull(Env::get('BAR'));
 		$this->assertEquals('VALUE3', Env::get('BAZ'));
 	}
 
-	public function test_it_handle_mismatch_configs()
+	#[Test]
+	public function it_handle_mismatch_configs(): void
 	{
-		$content = "# \n FOO= \n BAR \n =VALUE3 \n =";
-		$this->makeDotEnvFile($content);
+		$this->storage->write("# \n FOO= \n BAR \n =VALUE3 \n =");
 
 		$allEnvVars = getenv();
 
-		Env::load($this->dotEnvPath);
+		Env::load($this->storage);
 
 		$allEnvVars['FOO'] = '';
 		$this->assertEquals($allEnvVars, getenv());
 	}
 
-	private function makeDotEnvFile(string $content)
-	{
-		File::open(Disk::Local, $this->dotEnvPath)
-			->write($content);
-	}
-
-	private function removeEnvVariables(...$vars)
+	private function removeEnvVariables(...$vars): void
 	{
 		foreach ($vars as $var) {
 			putenv($var);
