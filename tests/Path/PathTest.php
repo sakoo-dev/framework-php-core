ed1fe47c52c06469ba721076e5a5077ea4941a45
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2025-05-24 13:01:22 +0330

    feat: add path module
    Signed-off-by: Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>

--- tests/Path/PathTest.php
+++ tests/Path/PathTest.php
@@ -1,51 +1,77 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sakoo\Framework\Core\Tests\Path;
 
+use PHPUnit\Framework\Attributes\Test;
 use Sakoo\Framework\Core\Path\Path;
 use Sakoo\Framework\Core\Tests\TestCase;
-use Symfony\Component\Finder\Finder;
 
-class PathTest extends TestCase
+final class PathTest extends TestCase
 {
 	private string $root;
 
-	public function setUp(): void
+	protected function setUp(): void
 	{
 		parent::setUp();
 		$this->root = realpath(__DIR__ . '/../../');
 	}
 
-	public function test_it_returns_root_dir_path_properly()
+	#[Test]
+	public function it_returns_root_dir_path_properly(): void
 	{
 		$this->assertEquals($this->root, Path::getRootDir());
 	}
 
-	public function test_it_returns_core_dir_path_properly()
+	#[Test]
+	public function it_returns_core_dir_path_properly(): void
 	{
 		$this->assertEquals("{$this->root}/src", Path::getCoreDir());
 	}
 
-	public function test_it_returns_core_vendor_path_properly()
+	#[Test]
+	public function it_returns_core_vendor_path_properly(): void
 	{
 		$this->assertEquals("{$this->root}/vendor", Path::getVendorDir());
 	}
 
-	public function test_it_returns_core_storage_path_properly()
+	#[Test]
+	public function it_returns_core_storage_path_properly(): void
 	{
 		$this->assertEquals("{$this->root}/storage", Path::getStorageDir());
 	}
 
-	public function test_it_returns_core_logs_path_properly()
+	#[Test]
+	public function it_returns_core_logs_path_properly(): void
 	{
 		$this->assertEquals("{$this->root}/storage/logs", Path::getLogsDir());
 	}
 
-	public function test_it_returns_all_php_files_properly()
+	#[Test]
+	public function it_returns_temp_test_path_properly(): void
 	{
-		$finder = Path::getProjectPHPFiles();
+		$this->assertEquals('/tmp/sakoo-test', Path::getTempTestDir());
+	}
 
-		$this->assertInstanceOf(Finder::class, $finder);
-		$this->assertGreaterThan(20, $finder->count());
+	#[Test]
+	public function it_returns_all_php_files_properly(): void
+	{
+		$files = Path::getProjectPHPFiles();
+		$this->assertGreaterThan(20, count($files));
+	}
+
+	#[Test]
+	public function it_returns_php_core_files(): void
+	{
+		$files = Path::getCorePHPFiles();
+		$this->assertGreaterThan(20, count($files));
+	}
+
+	#[Test]
+	public function it_returns_php_files_of_somewhere(): void
+	{
+		$files = Path::getPHPFilesOf($this->root);
+		$this->assertGreaterThan(20, count($files));
 	}
 }
